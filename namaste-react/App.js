1dc7046331bae164a57702038da76affa3872d1f
Harshitha DS <124554642+DsHarshitha@users.noreply.github.com>
2025-01-13 21:03:20 +0530

    Add files via upload

---
+++ namaste-react/App.js
@@ -0,0 +1,20 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm a H1 tag"),
+    React.createElement("h2", {}, "I'm a H2 tag"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
