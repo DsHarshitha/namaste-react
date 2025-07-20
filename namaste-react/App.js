fa8434e2e2d385a3b34c6c6ccc5ba1bfd907dcbc
HarshithaDS <harshithads21@gmail.com>
2025-07-20 16:06:20 +0530

    EP: 1 inception 1. How React is imported to ur project 2. How to write react code without JSX

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,20 +1,60 @@
+
+/** 
+ * TODO: This is the html structure we want to create using React
+ * <div id ='parent'>
+ * <div id ='child 1'> 
+ * <h1>I'm h1 tag</h1>
+ * <h2>I'm h2 tag</h2>
+ * </div>
+ * <div id ='child 2'> 
+ * <h1>I'm h1 tag</h1>
+ * <h2>I'm h2 tag</h2>
+ * </div>
+ * </div>
+ * 
+ * 
+ * 
+ * React.createElement(type, [props], ...[children]) // syntax
+ */
+
+
+
+//ReactElement(object) => becomes an HTML(Browser Understands)
+
+// Parent = react element
 const parent = React.createElement(
+  //type
   "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
+
+  // props
+  { id: "parent" }, 
+  
+  //children
+  React.createElement("div", { id: "child1" }, [  // To create siblings create array 
+    React.createElement("h1", {}, "I'm a H1 tag"),
+    React.createElement("h2", {}, "I'm a H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [  // To create siblings create array 
     React.createElement("h1", {}, "I'm a H1 tag"),
     React.createElement("h2", {}, "I'm a H2 tag"),
   ])
 );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
+/** The above code is tooo complicated to write.
+ * But this is how the CORE REACT code looks like.
+ * Therefore we use JSX to make our lives easier
+ * NOTE: We can always write React code without JSX but it will be complicated
+ * 
+*/
 
-console.log(heading);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+
+console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(parent); // this is just putting the parent in the "root" tag in index.html file
