67ab246241866d75038a00a293826717e6d65d30
HarshithaDS <harshithads21@gmail.com>
2025-08-23 23:47:49 +0530

    EP 3: Laying foundation part 1

--- App.js
+++ App.js
@@ -1,58 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/** 
- * TODO: This is the html structure we want to create using React
- * <div id ='parent'>
- * <div id ='child 1'> 
- * <h1>I'm h1 tag</h1>
- * <h2>I'm h2 tag</h2>
- * </div>
- * <div id ='child 2'> 
- * <h1>I'm h1 tag</h1>
- * <h2>I'm h2 tag</h2>
- * </div>
- * </div>
- * 
- * 
- * 
- * React.createElement(type, [props], ...[children]) // syntax
- */
+
+//const heading = React.createElement( tag we want to create, Attributes , child)
+// This is hpw we create element by React
+const heading = React.createElement(
+  "h1",
+  {id : "heading"},
+  "Namaste React"
+); 
+
+// console.log(heading);
 
 
+// JSX - HTML like or XML-like syntax
+// This is how we create element using JSX
+const jsxHeading = <h1 id = "heading">Namaste from jsx</h1> //<- This is same as the above React.createElement
 
-//ReactElement(object) => becomes an HTML(Browser Understands)
+// Java script/ js engine does not understand jsx [The above code]
+// Js engine only understands Ecma script/ ES6
+// Then how is the above code working??
+// Parcel is doing transpilation
+// jsx (Transpiled before it reaches javascript engine) - Parcel [Manages other modules to transpile doesnot do itself] - Babel (does the actual transpilation)
+// Babel - Takes Jsx code and converts into a script that REact/ browsers understand
+// Babel (not creatd by Facebook)
 
-// Parent = react element
-const parent = React.createElement("div", { id: "parent" },
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { key: "h1-1" }, "Namaste React"),
-    React.createElement("h2", { key: "h2-1" }, "I'm a H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { key: "h1-2" }, "I'm a H1 tag"),
-    React.createElement("h2", { key: "h2-2" }, "I'm a H2 tag"),
-  ])
-);
+const root = ReactDOM.createRoot(document.getElementById("root")); // 
 
-/** The above code is tooo complicated to write.
- * But this is how the CORE REACT code looks like.
- * Therefore we use JSX to make our lives easier
- * NOTE: We can always write React code without JSX but it will be complicated
- * 
-*/
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-
-console.log(parent); //object
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); // this is just putting the parent in the "root" tag in index.html file
-
-
-// This Code is not ready to push to Production
+root.render(jsxHeading);
