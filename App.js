c61e14ac67ca69fe718108896df48af8ffec6153
HarshithaDS <harshithads21@gmail.com>
2025-08-24 00:29:19 +0530

    ep 3:
    1. React Components
    2. Example of Functional component
    3. Component Composition

--- App.js
+++ App.js
@@ -2,29 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-//const heading = React.createElement( tag we want to create, Attributes , child)
-// This is hpw we create element by React
-const heading = React.createElement(
-  "h1",
-  {id : "heading"},
-  "Namaste React"
-); 
 
-// console.log(heading);
+// React Element
+const heading = <h1 id = "heading">Namaste from jsx</h1>;
 
+// React Functional component = def : a function that returns a jsx code
+const HeadingComponent = () => {
+  return <h2>Namaste from Functional component 1</h2>;
+}
 
-// JSX - HTML like or XML-like syntax
-// This is how we create element using JSX
-const jsxHeading = <h1 id = "heading">Namaste from jsx</h1> //<- This is same as the above React.createElement
+// Smae as above function == same as how we write arrow functions in javascript
+//here for the HeadingComponent - Babel will understand that this is a functional component and replace with the above function
+const HeadingComponent2 = () => (
+  <div><HeadingComponent/>
+<h1>Namaste from Functional componnet </h1>
+  </div>
+); // --> with Babel supported wrap
 
-// Java script/ js engine does not understand jsx [The above code]
-// Js engine only understands Ecma script/ ES6
-// Then how is the above code working??
-// Parcel is doing transpilation
-// jsx (Transpiled before it reaches javascript engine) - Parcel [Manages other modules to transpile doesnot do itself] - Babel (does the actual transpilation)
-// Babel - Takes Jsx code and converts into a script that REact/ browsers understand
-// Babel (not creatd by Facebook)
+// Component Composition
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(heading);
+
+// root.render(HeadingComponent) ==> we cant do this, this can be done only for React elements not React components
+// But 
+
+root.render(<HeadingComponent2/>);
+// Babel understands that there is some angular brackets and serach for the component and conbverts to a reactelemnt
