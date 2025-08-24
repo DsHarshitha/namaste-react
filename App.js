3560d80e8718ba62c25c03c99b8db62f1ccd90e5
HarshithaDS <harshithads21@gmail.com>
2025-08-24 17:52:20 +0530

    Ep 4: Code
    1. Planning basixc layout for Food delivery app
    2. Adding Header, body with some Css

--- App.js
+++ App.js
@@ -1,31 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * Project - 1 : Food Delivery app plan
+ * Header
+ *   - Logo
+ *   - Nav Items
+ * Body
+ *  - Search 
+ *  - Resto Container
+ *      - Resto Cards [Food pic, Restaurant name, cuisine, delivery time, rating]
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
 
+const Header = () => {
+  return (
+    <div className = "header">
+      <div className = "logo-container">
+        <img className="logo" src = "https://static.vecteezy.com/system/resources/previews/047/171/152/non_2x/logo-design-for-restaurant-and-food-company-vector.jpg"></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
 
-// React Element
-const heading = <h1 id = "heading">Namaste from jsx</h1>;
-
-
-const HeadingComponent = () => {
-  return <h2>Namaste from Functional component 1</h2>;
+  );
 }
 
-// To write react element/ javascript inside a Functinal component - use {}
-const HeadingComponent2 = () => (
-  <div>
-    {heading}
-    {100 + 300}
-    {console.log(10)}
-    <HeadingComponent/>
-    {HeadingComponent()}
-    <HeadingComponent></HeadingComponent>
-<h1>Namaste from Functional componnet </h1>
-  </div>
+const Body =() => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestoCard/>
+        <RestoCard/>
+        <RestoCard/>
+        <RestoCard/>
+        <RestoCard/>
+        <RestoCard/>
+        <RestoCard/>
+      </div>
+    </div>
+  );
+};
+
+
+const RestoCard = () => {
+return (
+<div className="res-card" style={{
+  backgroundColor: "#dbe1ceeb",
+}}>
+<img className="card-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlPQXFO4B201Ry8o7_Soa7dwhJBrtcwJdE0w&s"></img>
+<h3>Meghana Foods</h3>
+<h3>Biryani, Hyderabadi, Andra style </h3>
+<h3>4.4 ratings</h3>
+<h3>38 minutes</h3>
+</div>
 );
+};
+
+const AppLayout = () => {
+  return (
+    <div className = "app">
+      <Header/>
+      <Body/>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
-root.render(<HeadingComponent2/>);
+root.render(<AppLayout/>);
