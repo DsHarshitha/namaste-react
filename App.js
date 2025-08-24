cf1f4d56860bb673396fa6f30a94ad2aec4377ea
HarshithaDS <harshithads21@gmail.com>
2025-08-24 13:21:45 +0530

    ep: 3 part 5

--- App.js
+++ App.js
@@ -6,27 +6,26 @@ import ReactDOM from "react-dom/client";
 // React Element
 const heading = <h1 id = "heading">Namaste from jsx</h1>;
 
-// React Functional component = def : a function that returns a jsx code
+
 const HeadingComponent = () => {
   return <h2>Namaste from Functional component 1</h2>;
 }
 
-// Smae as above function == same as how we write arrow functions in javascript
-//here for the HeadingComponent - Babel will understand that this is a functional component and replace with the above function
+// To write react element/ javascript inside a Functinal component - use {}
 const HeadingComponent2 = () => (
-  <div><HeadingComponent/>
+  <div>
+    {heading}
+    {100 + 300}
+    {console.log(10)}
+    <HeadingComponent/>
+    {HeadingComponent()}
+    <HeadingComponent></HeadingComponent>
 <h1>Namaste from Functional componnet </h1>
   </div>
-); // --> with Babel supported wrap
-
-// Component Composition
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
 
-// root.render(HeadingComponent) ==> we cant do this, this can be done only for React elements not React components
-// But 
 
 root.render(<HeadingComponent2/>);
-// Babel understands that there is some angular brackets and serach for the component and conbverts to a reactelemnt
